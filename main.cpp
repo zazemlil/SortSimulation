e2ca77127180fb3f7fcd44035871826807a78fd7
zazemlil <89774967+zazemlil@users.noreply.github.com>
2023-01-24 19:53:04 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
 #include <SFML/Graphics.hpp>
-#include <ctime>
-#include <Windows.h>
+#include <thread>
 #include <iostream>
 
 using namespace sf;
 
-const int WIDTH = 800;
-const int HEIGHT = 600;
+const int WIDTH = 1200;
+const int HEIGHT = 900;
 
 class SortSimulation
 {
@@ -17,6 +16,8 @@ private:
 	float sizeofitemX, sizeofitemY;
 	int maxelem;
 	int k, f = 1;
+	float sizeofheaderX = 0.f;
+	float sizeofheaderY = 0.f;
 	RenderWindow* window;
 	
 	int MaxOfArray(int* array, int size)
@@ -33,7 +34,7 @@ private:
 	{
 		Clock clock;
 		float time = 0;
-		float x = 0.01f / speed;
+		float x = 100000.f / speed;
 
 		while (time < x)
 		{
@@ -45,11 +46,10 @@ private:
 	{
 		if (reverse == 1)
 		{
-			for (int i = 0; i < 100; i++)
+			for (int i = 0; i < size; i++)
 			{
-				for (int j = i; j < 100; j++)
+				for (int j = i; j < size; j++)
 				{
-
 					if (*(array + i) < *(array + j))
 					{
 						k = *(array + i);
@@ -57,14 +57,15 @@ private:
 						*(array + j) = k;
 					}
 					Draw();
+					Timer(speed);
 				}
 			}
 		}
 		else
 		{
-			for (int i = 0; i < 100; i++)
+			for (int i = 0; i < size; i++)
 			{
-				for (int j = i; j < 100; j++)
+				for (int j = i; j < size; j++)
 				{
 
 					if (*(array + i) > *(array + j))
@@ -74,8 +75,152 @@ private:
 						*(array + j) = k;
 					}
 					Draw();
+					Timer(speed);
 				}
 			}
+		}
+	}
+
+	void ChoiceSort(int reverse)
+	{
+		int mn, kk;
+		if (reverse == 1)
+		{
+			for (int i = 0; i < size-1; i++)
+			{
+				mn = i;
+				for (int j = i+1; j < size; j++)
+				{
+					if (*(array + j) > *(array + mn))
+					{
+						mn = j;
+					}
+					Draw();
+					Timer(speed);
+				}
+				kk = *(array + i);
+				*(array + i) = *(array + mn);
+				*(array + mn) = kk;
+			}
+		}
+		else
+		{
+			for (int i = 0; i < size-1; i++)
+			{
+				mn = i;
+				for (int j = i+1; j < size; j++)
+				{
+					if (*(array + j) < *(array + mn))
+					{
+						mn = j;
+					}
+					Draw();
+					Timer(speed);
+				}
+				kk = *(array + i);
+				*(array + i) = *(array + mn);
+				*(array + mn) = kk;
+			}
+		}
+	}
+
+	int partition(int reverse, int start, int pivot)
+	{
+		int i = start;
+		if (reverse == 1)
+		{
+			while (i < pivot)
+			{
+				if (*(array + i) < *(array + pivot) && i == pivot - 1)
+				{
+					std::swap(*(array + i), *(array + pivot));
+					pivot--;
+				}
+				else if (*(array + i) < *(array + pivot))
+				{
+					std::swap(*(array + pivot - 1), *(array + pivot));
+					std::swap(*(array + i), *(array + pivot));
+					pivot--;
+				}
+				else i++;
+				Draw();
+				Timer(speed);
+			}
+		}
+		else
+		{
+			while (i < pivot)
+			{
+				if (*(array + i) > *(array + pivot) && i == pivot - 1)
+				{
+					std::swap(*(array + i), *(array + pivot));
+					pivot--;
+				}
+				else if (*(array + i) > *(array + pivot))
+				{
+					std::swap(*(array + pivot - 1), *(array + pivot));
+					std::swap(*(array + i), *(array + pivot));
+					pivot--;
+				}
+				else i++;
+				Draw();
+				Timer(speed);
+			}
+		}
+		
+		return pivot;
+	}
+
+	void QuickSort(int reverse, int start, int end)
+	{
+		if (start < end)
+		{
+			int pivot = partition(reverse, start, end);
+
+			QuickSort(reverse, start, pivot - 1);
+			QuickSort(reverse, pivot + 1, end);
+		}
+	}
+
+	void heapify(int reverse, int list[], int listLength, int root)
+	{
+		int largest = root;
+		int l = 2 * root + 1;
+		int r = 2 * root + 2;
+		if (reverse == 1)
+		{
+			if (l < listLength && list[l] < list[largest])
+				largest = l;
+
+			if (r < listLength && list[r] < list[largest])
+				largest = r;
+		}
+		else
+		{
+			if (l < listLength && list[l] > list[largest])
+				largest = l;
+
+			if (r < listLength && list[r] > list[largest])
+				largest = r;
+		}
+		if (largest != root)
+		{
+			std::swap(list[root], list[largest]);
+			heapify(reverse, list, listLength, largest);
+		}
+		Draw();
+		Timer(speed);
+	}
+
+	void HeapSort(int reverse, int list[], int listLength)
+	{
+		for (int i = listLength / 2 - 1; i >= 0; i--)
+			heapify(reverse, list, listLength, i);
+
+		for (int i = listLength - 1; i >= 0; i--)
+		{
+			std::swap(list[0], list[i]);
+			heapify(reverse, list, i, 0);
 		}
 	}
 
@@ -88,31 +233,38 @@ private:
 			{
 				*(array + i) = rand() % 1000;
 			}
+			maxelem = MaxOfArray(array, size);
 			Draw();
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Space) && f == 1)
 		{
-			BubleSort(0);
+			//BubleSort(0);
+			//ChoiceSort(0);
+			//QuickSort(0, 0, size);
+			HeapSort(0, array, size);
 			f = 0;
+			Timer(0.000001f);
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Space) && f == 0)
 		{
-			BubleSort(1);
+			//BubleSort(1);
+			//ChoiceSort(1);
+			//QuickSort(1, 0, size);
+			HeapSort(1, array, size);
 			f = 1;
+			Timer(0.000001f);
 		}
+		Draw();
 	}
 
 	void Draw()
 	{
 		window->clear(sf::Color::Cyan);
+		sprite.setScale(Vector2f(sizeofitemX / 12.f, float(HEIGHT)));
+		Vector2f v = sprite.getScale();;
 		for (int i = 0; i < size; i++)
 		{
-			Timer(speed);
-			sprite.setScale(Vector2f(100.f/float(WIDTH),float(HEIGHT)));
-			sprite.setPosition(float(i)*7.f, float(HEIGHT) - *(array+i)/sizeofitemY);
-			//sprite.setScale(Vector2f(32.f / (float(WIDTH) / float(size)), float(HEIGHT)));
-			////sprite.setPosition(float(i) * float(WIDTH) / float(size), float(HEIGHT) - *(array+i)/sizeofitemY);
-			//sprite.setPosition(float(i) * float(WIDTH) / float(size) / 50.f, float(HEIGHT) - *(array + i) / sizeofitemY);
+			sprite.setPosition(float(i) * v.x * 12.f, float(HEIGHT) - *(array + i) / sizeofitemY);
 			window->draw(sprite);
 		}
 		window->display();
@@ -125,16 +277,19 @@ public:
 		this->speed = speed;
 		this->array = array;
 		this->sprite = sprite;
+		this->sprite.setTextureRect(IntRect(300, 0, 10, 600));
 		this->window = window;
 		maxelem = MaxOfArray(array, size);
-		sizeofitemX = float(WIDTH) / float(size);
-		sizeofitemY = float(maxelem)/float(HEIGHT);
-		Draw();
+		sizeofitemX = float(WIDTH - sizeofheaderX) / float(size);
+		sizeofitemY = float(maxelem)/float(HEIGHT - sizeofheaderY);
 	}
 
 	void update()
 	{
-		Controller();
+		while (window->isOpen())
+		{
+			Controller();
+		}
 	}
 };
 
@@ -150,13 +305,16 @@ int main()
 	itemTexture.loadFromImage(itemImg);
 	Sprite itemSprite;
 	itemSprite.setTexture(itemTexture);
+	
 
-	int size = 100;
-	float speed = 0.1f;
+	int size = 500;
+	float speed = 2.5f;
 	int* array = new int[size];
 	for (int i = 0; i < size; i++) *(array + i) = rand()%1000;
 
 	SortSimulation massiv(array, size, speed, itemSprite, pwindow);
+	window.setActive(false);
+	std::thread t(&SortSimulation::update, massiv);
 
 	while (window.isOpen())
 	{
@@ -166,9 +324,9 @@ int main()
 			if (event.type == Event::Closed)
 				window.close();
 		}
-		massiv.update();
 	}
+	t.join();
 	return 0;
 }
-//не могу подобрать разрешение для спрайта
-//мб нужны потоки
+//нужны потоки!!!!!!
+//нужно сделать меню
